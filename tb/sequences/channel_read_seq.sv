94a0eb81dfa9572f4dda3565ffc7f67f571dbe27
yonizvida-gif <yonizvida@gmail.com>
2026-09-14 17:47:20 +0300

    Update channel_read_seq.sv

--- tb/sequences/channel_read_seq.sv
+++ tb/sequences/channel_read_seq.sv
@@ -20,9 +20,7 @@ class channel_read_seq extends uvm_sequence #(reg_tran);
         for(int i = 0; i < 4; i++) begin
 
 
-            tr = reg_tran::type_id::create(
-                    $sformatf("read_channel_%0d", i)
-                 );
+            tr = reg_tran::type_id::create($sformatf("read_channel_%0d", i));
 
 
             start_item(tr);
@@ -31,9 +29,8 @@ class channel_read_seq extends uvm_sequence #(reg_tran);
                 kind    == 1'b1;    // READ
                 address == i;
             })
-                `uvm_error("CHANNEL_READ_SEQ",
-                           "Channel READ randomization failed")
-
+                `uvm_error("CHANNEL_READ_SEQ", "Channel READ randomization failed")
+                        
             finish_item(tr);
 
 
@@ -44,21 +41,14 @@ class channel_read_seq extends uvm_sequence #(reg_tran);
             channel_data[i] = rsp.data;
 
 
-            `uvm_info("CHANNEL_READ_SEQ",
-                $sformatf(
-                    "Channel %0d READ: data = 0x%0h",
-                    i,
-                    rsp.data
-                ),
-                UVM_LOW)
+            `uvm_info("CHANNEL_READ_SEQ", $sformatf("Channel %0d READ: data = 0x%0h", i, rsp.data), UVM_LOW)         
+                
 
         end
 
 
-        `uvm_info("CHANNEL_READ_SEQ",
-                  "Finished reading Channel0-3",
-                  UVM_LOW)
-
+        `uvm_info("CHANNEL_READ_SEQ", "Finished reading Channel0-3", UVM_LOW)
+                 
     endtask
 
 
